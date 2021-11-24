a7de7d9f0636537e3ff55c80cbf8865554e9de48
mina <mina72292@gmail.com>
2021-11-24 14:16:20 +0200

    mina changes

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -1,144 +1,160 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package uper;
+
+
+package uper.src.com.company;
 
 import java.util.Scanner;
 
 public class Client extends User implements SignUp {
     private Request request;
+
     public Client(String userName, String email, String password, String mobileNumber) {
         super(userName, email, password, mobileNumber);
     }
-    
+
     public Client(String email, String password) {
         super(email, password);
     }
-    
-  
 
-    public void requestRide(String source,String destination)
-    {
-        Area src=new Area(source);
-        Area dest =new Area(destination);
-        request=new Request(src,dest,this);
+
+    public void requestRide(String source, String destination) {
+        Area src = new Area(source);
+        Area dest = new Area(destination);
+        request = new Request(src, dest, this);
     }
 
-    public void rateDriver(Driver d, double rate){
+    public void rateDriver(Driver d, double rate) {
         d.rate.addRate(rate);
         d.rate.setRate(rate);
     }
 
-    public Boolean Register(){
-        boolean check =false;
-        for(int i=0;i<d.SuspUser.size();i++){
-            if(d.SuspUser.get(i)==this.email){
-                check=true;
+    public Boolean Register() {
+        boolean check = false;
+        for (int i = 0; i < d.SuspUser.size(); i++) {
+            if (d.SuspUser.get(i) == this.email) {
+                check = true;
             }
         }
-        if(!check){
+        if (!check) {
             d.ClientList.add((Client) this);
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    public  int logIn() {
-        boolean check =true;
-        for(int i=0;i<d.SuspUser.size();i++){
-            if(d.SuspUser.get(i)==this.email){
-                check=false;
+    public int logIn() {
+        boolean check = true;
+        for (int i = 0; i < d.SuspUser.size(); i++) {
+            if (d.SuspUser.get(i) == this.email) {
+                check = false;
             }
         }
         boolean flag = false;
-        for(int i=0; i< d.ClientList.size() ;i++)
-        {
-            if(this.email.equals(d.ClientList.get(i).email)&& this.password.equals(d.ClientList.get(i).password))
+        for (int i = 0; i < d.ClientList.size(); i++) {
+            if (this.email.equals(d.ClientList.get(i).email) && this.password.equals(d.ClientList.get(i).password))
                 flag = true;
         }
-        if(check==false&&flag==false){
+        if (check == false && flag == false) {
             return 1;
-        }
-        else if(check==true&&flag==true){
+        } else if (check == true && flag == true) {
             return 2;
-        }
-        else{
+        } else {
             return 3;
         }
     }
-    public void ViewOffer(){
-        Scanner sc=new Scanner(System.in);
-        for(int i=0;i<request.listOffer.size();i++){
-            System.out.println("Number of offer: "+(i+1));
-            System.out.println(request.listOffer.get(i));
+
+    public void ViewOffer() throws NullPointerException {
+        try {
+            Scanner sc = new Scanner(System.in);
+
+            for (int i = 0; i < request.listOffer.size(); i++) {
+                System.out.println("Number of offer: " + (i + 1));
+                System.out.println(request.listOffer.get(i));
+            }
+            System.out.println("Enter Number of Offer to accept");
+            int n = sc.nextInt();
+            request.listOffer.get(n - 1).accept = true;
+            System.out.println("Do you want to set Rate ?(Yes/No)");
+            String s = sc.next();
+            if (s.equalsIgnoreCase("Yes")) {
+                System.out.println("Enter the rate");
+                double rate = sc.nextDouble();
+                this.rateDriver(request.listOffer.get(n - 1).driver, rate);
+            }
+            request.listOffer.clear();
+        } catch (NullPointerException e) {
+            System.out.println("No offers till now");
         }
-        System.out.println("Enter Number of Offer to accept");
-        int n= sc.nextInt();
-        request.listOffer.get(n-1).accept=true;
-        System.out.println("Do you want to set Rate ?(Yes/No)");
-        String s = sc.next();
-        if(s.equalsIgnoreCase("Yes")) {
-            System.out.println("Enter the rate");
-            double rate = sc.nextDouble();
-            this.rateDriver(request.listOffer.get(n - 1).driver, rate);
-        }
-        request.listOffer.clear();
+
+
     }
-    public void rateAnyDriver(){
-        Database d = new Database();
-        for (int i=0; i<d.DriverList.size(); i++){
-            System.out.println("Driver "+ (i+1));
-            System.out.println(d.DriverList.get(i));
+
+    public void rateAnyDriver() throws IndexOutOfBoundsException {
+        try {
+            Database d = new Database();
+            for (int i = 0; i < d.DriverList.size(); i++) {
+                System.out.println("Driver " + (i + 1));
+                System.out.println(d.DriverList.get(i));
+            }
+            System.out.println(d.DriverList.size());
+            if (d.DriverList.size() == 0) {
+                System.out.println("No drivers available");
+                return;
+            } else {
+                System.out.println("Do you want to rate any Driver ?(yes/no)");
+                Scanner sc = new Scanner(System.in);
+                String s = sc.next();
+                if ("yes".equalsIgnoreCase(s)) {
+                    System.out.println("Choose the Driver number.");
+                    int id = sc.nextInt();
+                    System.out.println("Enter the rate");
+                    double r = sc.nextDouble();
+                    this.rateDriver(d.DriverList.get(id - 1), r);
+                }
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("No drivers available");
         }
-        System.out.println("Do you want to rate any Driver ?(yes/no)");
-        Scanner sc=new Scanner(System.in);
-        String s = sc.next();
-        if ("yes".equalsIgnoreCase(s)){
-            System.out.println("Choose the Driver number.");
-            int id = sc.nextInt();
-            System.out.println("Enter the rate");
-            double r = sc.nextDouble();
-            this.rateDriver(d.DriverList.get(id-1), r);
-        }
+
     }
-    public void clientInterface(){
-        while (true){
+
+    public void clientInterface() {
+        while (true) {
             System.out.println("Client's List");
             System.out.println("press 1 to request new ride.");
             System.out.println("press 2 to list all offers.");
             System.out.println("press 3 to rate Drivers.");
             System.out.println("press 4 to exit");
-            Scanner sc=new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);
             int x = sc.nextInt();
-            switch (x){
+            switch (x) {
                 case 1 -> {
                     System.out.println("Enter the source.");
                     String src = sc.next();
                     System.out.println("Enter the destination");
                     String dest = sc.next();
-                    this.requestRide(src , dest);
+                    this.requestRide(src, dest);
                 }
                 case 2 -> this.ViewOffer();
                 case 3 -> this.rateAnyDriver();
-                case 4 -> {break;}
-            
+                case 4 -> {
+                    break;
+                }
+
             }
-            if(x == 4)
+            if (x == 4)
                 break;
         }
     }
+
     @Override
     public String toString() {
-        return "Client:" +'\n'+
+        return "Client:" + '\n' +
                 "   userName=" + userName + '\n' +
                 "   email=" + email + '\n' +
                 "   mobileNumber=" + mobileNumber + '\n' +
-                "   ID=" + ID +'\n'+
-                '}'+'\n';
+                "   ID=" + ID + '\n' +
+                '}' + '\n';
     }
 
 }
