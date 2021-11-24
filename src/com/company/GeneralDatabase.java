7c016a95807c410e70ba55267e9a8b0a174d03d8
josephdiaa <joseph.diaa@gmail.com>
2021-11-24 21:10:39 +0200

    General Data Base

---
+++ src/com/company/GeneralDatabase.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public interface GeneralDatabase {
+    /**
+     * update to database
+     */
+    public void update();
+
+
+    /**
+     * delete from database
+     */
+    public void delete();
+
+    /**
+     * set to database
+     */
+    public void set();
+
+    /**
+     * get from database
+     */
+    public void get();
+}
